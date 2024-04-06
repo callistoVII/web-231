cd646e3783f7f29ad4a03f5f0e61560d2983ebc2
Nyx <xamarynth@gmail.com>
2024-04-06 13:42:54 -0400

    Week 2 Edit.

---
+++ week-2/chapter/js02_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Niki Nielsen
+      Date: 03/25/2024
+
+      Filename: js02.js
+ */
+
---
+++ week-2/project02-02/project02-02_txt.js
@@ -0,0 +1,11 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Niki Nielsen
+      Date: 04/01/2024
+
+      Filename: project02-02.js
+ */
+
